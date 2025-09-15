6c398038b52a9276277dba501729de2c36fda625
saikumar <wwwsaikumar150@gmail.com>
2025-09-15 17:40:36 +0530

    devops conversation

--- installation.sh
+++ installation.sh
@@ -4,7 +4,7 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
 then 
-     ech0 "please run this scipt with root access"
+     echo "please run this scipt with root access"
      exit 1
 else
      echo "you are a super user"
