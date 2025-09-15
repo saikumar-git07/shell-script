17e5bea11336bf02a93b69740ca89e184eb2b8ed
saikumar <wwwsaikumar150@gmail.com>
2025-09-15 15:52:54 +0530

    devops conversation

--- conditions.sh
+++ conditions.sh
@@ -4,7 +4,7 @@ NUMBER=5
 
 if [$NUMBER -gt 10]
 then
-    echo "given number $NUMBER is greater than 10"
+     echo "given number $NUMBER is greater than 10"
 else 
-    echo "given number $NUMBER is less than 10"
+     echo "given number $NUMBER is less than 10"
  fi   
