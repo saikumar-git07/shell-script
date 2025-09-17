488eec6475295ccfa00e25506764c4b15c5e95bf
saikumar <wwwsaikumar150@gmail.com>
2025-09-17 17:33:00 +0530

    functions

--- functions.sh
+++ functions.sh
@@ -5,9 +5,10 @@ USERID=$(id -u)
 VALIDATE(){
     if [ $1 -ne 0 ]
     then 
-    echo "$2 is....FAILURE"
-    exit 1
-    echo "$2 is....SUCCESS"
+        echo "$2 is....FAILURE"
+        exit 1
+        echo "$2 is....SUCCESS"
+    fi
 }
 
 if [ $USERID -ne 0 ]
