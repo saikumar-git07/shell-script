4b121d82df162ea2b4b78023b2f3f9b685abaf0f
saikumar <wwwsaikumar150@gmail.com>
2025-09-17 15:45:51 +0530

    devops conversation

---
+++ functions.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+VLIDATE(){
+
+}
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with root access"
+    exit 1
+else 
+    echo "you are super user"
+fi
+
+dnf install mysql -y 
+VALIDATE $?  "installing MYSQL"
+
+dnf install git -y
+VALIDATE $?  "installing GIT"
+
+echo "is script is proceeding?"
