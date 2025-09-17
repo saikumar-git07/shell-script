ffcc4f38b36c952c5db8a8b59861acfe89387aca
saikumar <wwwsaikumar150@gmail.com>
2025-09-17 21:39:42 +0530

    functions

--- install-packages.sh
+++ install-packages.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
 USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then 
+        echo -e "$2 is....FAILURE"
+        exit 1
+    else
+        echo -e "$2 is....SUCCESS"
+    fi
+}
+
 if [$USERID -ne 0 ]
 then 
     echo "please run this script with root access"
@@ -11,4 +25,10 @@ fi
 for i in $@
 do
    echo "Package to install: $i"
+   dnf list installed $i &>>$LOGFILE
+   if [ $? -eq 0 ]
+   then 
+       echo "$i Already installed...SKIPPING"
+   else 
+       echo "$i not installed...Need to install" 
 done
