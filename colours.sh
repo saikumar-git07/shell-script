64acf975e6cd45139d23ed1d2cff29dd3cddb34d
saikumar <wwwsaikumar150@gmail.com>
2025-09-17 19:47:59 +0530

    functions

--- colours.sh
+++ colours.sh
@@ -1,14 +1,21 @@
+#!/bin/bash
+
 USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="e\[31m"
+G="e\[32m"
+N="e\[0m"
+
+echo "script is started executing at: $TIMESTAMP"
 
 VALIDATE(){
     if [ $1 -ne 0 ]
     then 
-        echo -e "$2 is....FAILURE"
+        echo -e "$2 is....$R FAILURE $N"
         exit 1
-        echo -e "$2 is....SUCCESS"
+        echo -e "$2 is....$G SUCCESS $N"
     fi
 }
 
