69b3ae4ae67920166ec44ecae04ee649254c080c
saikumar <wwwsaikumar150@gmail.com>
2025-09-23 23:36:16 +0530

    devops conversation

--- set.sh
+++ set.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-set -e 
+set -e
 
 failure(){
-    echo "failed at $1: $2"
+    echo "Failed at $1: $2"
 }
 
-trap 'failure ${lineno} "$BASH_COMMAND"' ERR
+trap 'failure ${LINENO} "$BASH_COMMAND"' ERR
 
-USERID=$(id -u)
+USERID=$(id -u) #ERR
 
 if [ $USERID -ne 0 ]
-then 
-     echo "please run this script with root access"
-     exit 1
+then
+    echo "Please run this script with root access."
+    exit 1 # manually exit if error comes.
 else
-     echo "you are a super user"
+    echo "You are super user."
 fi
 
-dnf install mysqql -y
+dnf install mysfaffql -y
 dnf install git -y
 
-echo "is script is proceeding ?"
+echo "is script proceeding?"
