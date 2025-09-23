683b13f085b69601054b319a2506d4427dd077cc
saikumar <wwwsaikumar150@gmail.com>
2025-09-23 21:06:37 +0530

    devops conversation

---
+++ set.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then 
+     echo "please run this scipt with root access"
+     exit 1
+else
+     echo "you are a super user"
+fi
+
+dnf install mysqil -y
+dnf install git -y
+
+echo "is script is proceeding ?"
